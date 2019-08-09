750b803a2532e9f3e4bc0e246786f61c39521bc4
as <88452486@qq.com>
2019-08-09 13:41:45 +0800

    mybatis generator demo

---
+++ tm-task/src/main/java/com/taiping/tm/persistence/dao/StudentMapper.java
@@ -0,0 +1,16 @@
+package com.taiping.tm.persistence.dao;
+
+import com.taiping.tm.persistence.entity.Student;
+import java.util.List;
+
+public interface StudentMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Student record);
+
+    Student selectByPrimaryKey(Integer id);
+
+    List<Student> selectAll();
+
+    int updateByPrimaryKey(Student record);
+}
